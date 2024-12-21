41c561abfab293cec9ca35ba8a7be8b7ae6c813a
Alexis Gaudray Bouju <alexis.gaudray-bouju@etu.u-bordeaux.fr>
2024-12-21 21:38:41 +0100

    Ajout groupe UE

--- csv_to_dic.py
+++ csv_to_dic.py
@@ -1,6 +1,6 @@
 res = ""
 for semestre in range(5, 11):
-    res += f"semestre{semestre}" + " {\n"
+    res += f"semestre{semestre}:" + " {\n"
     for filiere in ['info', 'telecom', 'matmeca', 'elec', 'see', 'ri']:
         try:
             with open(f"./data/S{semestre}_{filiere}.csv", 'r', encoding='utf8') as file:
@@ -8,18 +8,27 @@ for semestre in range(5, 11):
         except FileNotFoundError:
             print(f"{semestre} {filiere} non trouvé")
         else:
-            res += f"    {filiere}: ["
-            for l in lines:
+            res += f"    {filiere}: " + '{'
+            for i, l in enumerate(lines):
                 arg = l.split(';')
-                if len(arg) >= 8 and arg[3] == "MODULE":
-                    nom = arg[4].replace('S5', '').replace('(', '').replace(')', '').repalce(',', '.')
-                    for i in range(10):
-                        nom = nom.replace(f"n°{i}", '')
-                    nom = nom.strip()
-                    res += '\n        {nom: "' + nom + '", note: "", coef: ' + arg[8] + '},'
-            res = res[:-1]
-            res += '\n    ],\n'
-    res = res[:-1]
-    res += '},\n'
+                if len(arg) < 8:
+                    continue
+                if arg[3] == "UE":
+                    if i > 2:
+                        res += "\n        ],"
+                    res += f'\n        "{" - ".join(arg[4].split(' - ')[1:]).strip()}": ['
+                elif arg[3] == "MODULE":
+                    coef = arg[8].replace(',', '.')
+                    if not coef:
+                        continue
+                    res += '\n            {nom: "' + arg[4].strip() + '", note: "", coef: ' + coef + '},'
+            if res[-1] == "[":
+                res = "\n".join(res.split('\n')[:-1])           
+            else:
+                res = res[:-1]
+                res += '\n        ]'
+            res += '\n    },\n'
+    res = res[:-2]
+    res += '\n},\n'
 res = res[:-1]
 print(res)
